e65ba0e8cf858db084282f2b83aeae21067b92fd
Jay Wood <jwoodcodes@gmail.com>
2023-11-13 20:39:39 -0600

    fin home work sec. and about section

--- src/app/page.js
+++ src/app/page.js
@@ -9,27 +9,62 @@ export default function Home() {
     <div className={styles.wholeMainPageWrapper}>
       <MainNav />
       <div className={styles.mainSectionWrapper}>
-      <div className={styles.nameAndTitleWrapper}>
-      <div className={styles.name}>Jay Wood</div>
-      <span className={styles.jobTitle}>Web Developer</span>
-      <div className={styles.mainSectionIntro}>
-        Hi there! I&apos;m Jay, a web developer from Missouri. I love to take concepts I wish existed and make them into a reality. I am so happy to share what i&apos;ve been working on with you!
+        <div className={styles.nameAndTitleWrapper}>
+        <h1 className={styles.name}>Jay Wood</h1>
+        <span className={styles.jobTitle}>Web Developer</span>
+        <div className={styles.mainSectionIntro}>
+           Hi there! I&apos;m Jay, a web developer from Missouri. I love to take concepts and them into a reality. I am so happy to share what i&apos;ve been working on with you!
         
       </div>
      </div>
      
       <div className={styles.mainImageContainer}>
+        <Image src='/coloredSmoke.jpg' cover width='2000' height='1200' className={styles.mainPageImage} alt='coloerd smoke lines image'/>
+      </div>
 
-      <Image src='/coloredSmoke.jpg' cover width='2000' height='1200' className={styles.mainPageImage} alt='coloerd smoke lines image'/>
       </div>
-      </div>
-      <div className={styles.projectsSectionWrapper}>
+
+      {/* projects section */}
+
+      <div className={styles.sectionDivider}></div>
+
+        <div className={styles.projectsSectionWrapper} id='projects'>
         <div className={styles.myWorkHeaderContainer}>
           <h2 className={styles.projectsHeader}>My Work</h2>
-         </div>
-        <CardContainer />
-        
-          
+        </div>
+        <CardContainer />          
+      </div>
+
+      {/* about section */}
+
+      <div className={styles.sectionDivider}></div>
+
+      <div className={styles.aboutSectionWrapper} id='about'>
+          <h2 className={styles.aboutSectionHeader}>About Me</h2>
+          <section className={styles.aboutMeMainSection}>
+            Hi, I&apos;m Jay and I am so happy you&apos;re here!
+            <br/>
+            <br/>
+            In early 2001 I stood in the kitchen of an empty restaurant looking out a window at a barren downtown. Nothing moved. There was a distinct feeling that the world as we knew it was coming to an end. I was listening to a favorite buddhism podcast and as the host relayed messages of when one door closes another door opens I was struck by the question. <br/>What door will I choose to open now that the world&apos;s closed it&apos;s door?
+            <br/>
+            <br/>
+            As I was looking forward and pondereing what was next, I decided to look back. I always had a curiosity about code since my high school days playing MUDD&apos;s and remembered the feeling of desperately wanting to know how to take an idea and turn it into an experience for someone on a screen. The world had moved on, so I was going to too. All seemed possible as I dove in with all the gusto of one with the wind at their backs and limitless futures before them.
+            <br/>
+            <br/>
+            Since that day my curiosity and excitement has only gained steam. I have taken things I thought could be better and built better versions. Taken ideas and tools I had mearly wished existed and made them into apps anyone in the world can pull up in front of them and use. Every time I pull up my code editor on a new project, I am back standing in front of that window, with a blank new world in front of me, and I <em>cannot wait</em> to share that geniuine joy with you!
+          </section>
+      </div>
+
+      {/* contact section */}
+
+      <div className={styles.sectionDivider}></div>
+
+      <div className={styles.contactSectionWrapper} id='contact'>
+        <h2 className={styles.contactSectionHeading}>Contact</h2>
+        <div>
+
+        </div>
+
       </div>
 
     </div>
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -6,8 +6,9 @@ export default function MainNav () {
     return (
        <div className={styles.mainNav}>
         <Link href='/' className={styles.navLink}>Home</Link>
-        <Link href='/' className={styles.navLink}>Projects</Link>
-        <Link href='/' className={styles.navLink}>About</Link>       
+        <Link href='#projects' className={styles.navLink}>Projects</Link>
+        <Link href='#about' className={styles.navLink}>About</Link> 
+        <Link href='#contact' className={styles.navLink}>Contact</Link>      
         </div> 
     )
 }
