ab08d47004a828f9ea089690316a934e9d915d64
J Wood <73401121+DFK333@users.noreply.github.com>
2023-11-22 16:31:10 -0600

    name change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,30 @@
-
-import React from 'react';
-import styles from '@/src/styles/footer.module.css'
-import Link from 'next/link'
-import Image from 'next/image';
-
+import React from "react";
+import styles from "@/src/styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 // import { Github } from 'react-feather'
 
-
-export default function Footer () {
-    return (
-        
-        <div className={styles.wholeWrapper}>
-            
-        <Link href='https://github.com/jwoodcodes'>
-          <Image
-            src='/github-mark-white.png'
-            width={60}
-            height={60}
-            alt="github link"
-          ></Image>
-        </Link>
-        <div className={styles.myName}>Jay Wood 2023</div>
-        <Link href='https://www.linkedin.com/in/jay-wood-131432263/'>
-          <Image
-            src='/linkedin.png'
-            width={80}
-            height={80}
-            alt="github link"
-          ></Image>
-        </Link>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className={styles.wholeWrapper}>
+      <Link href="https://github.com/jwoodcodes">
+        <Image
+          src="/github-mark-white.png"
+          width={60}
+          height={60}
+          alt="github link"
+        ></Image>
+      </Link>
+      <div className={styles.myName}>Jordan Wood 2023</div>
+      <Link href="https://www.linkedin.com/in/jay-wood-131432263/">
+        <Image
+          src="/linkedin.png"
+          width={80}
+          height={80}
+          alt="github link"
+        ></Image>
+      </Link>
+    </div>
+  );
 }
