83f50d0ae16f5e80302c83d917dfbeb2022a3c6a
Jay Wood <jwoodcodes@gmail.com>
2024-01-13 20:49:46 -0600

    added content to ebv page

--- src/app/ebv/page.js
+++ src/app/ebv/page.js
@@ -21,12 +21,12 @@ export default function EBV() {
                 <p className={styles.infoParagraph}>
 
                     <Link href='https://ebv.vercel.app/'  className={styles.siteLink}>Go to ElderBlossom View &rarr;</Link>
-                    <br /> <br />
-                    fdsfsd ds fsfjdsf sfjds k fdskf sdfjdklsfjslkfjsdkf dskfsd
-                    dkfj dslkfjdsklf sdklfj dskfjdslkf sdf jsdfsdfsd
-                    ksdfkds lfdslgsdjgkfdglsd gjlsd lfdskfsddklsg sdgflsd
-                    s fkdlsfds;gklfjgdgjs s s gsdl gdsgjsdg sd gds gsd;g schooldsg
-                    s gsdgdsgldls;gl
+                    <br />
+                    * ElderBlossom View is a business content and ecommerce site for an elderberry winery and event venue
+                    <br /><br />
+                    * features next.js dynamic routing, full cart functionality with local storage, static data fetching from MongoDb, and responsive tables
+                    <br/><br/>
+                    * Built using React, NextJs, MongoDb, CSS
                 </p>
             </div>
 
@@ -37,14 +37,28 @@ export default function EBV() {
             {/* section break */}
 
             <div className={styles.sectionWrapper}>
-                <p className={styles.infoParagraph}>dasf fsdfsd sdfds fsd dsfjdsjf sdhfdshfudsfo s fdsfsd fs sdf sdf schoolsdf
-                    sdf sdf sd sd sd gsdfsdf sd fsdf sd fsdd fsdf sdf sdhfdshfudsfos favorite
-                    sd fsdf sdfs sdfdsf dsf sdf dsfds fdfsf dsfdsf sdfdsf dsfdsf dsfds
-                     dsfds fsdfds fdsfds fdsfsd fdsfds fdsfds fdsf dsfdsf sdf
+                <p className={styles.infoParagraph}>
+                    * I built this because I know the owners of this winery and they are such beautiful people with a gorgous venue and products but their site design didn't show this to the world and I felt like I could do so
+                    <br /> <br />
+                    * My goal was to pick a color scheme that went with their products and to use a design that highlighted and showcased their venue and products and let them be the star
                 </p>
                 <Image src='/ebvScreenshot.png' width={900} height={500} alt="ebv project shop page" className={styles.siteScreenshots}></Image>
             
             </div>
+
+            <div className={styles.sectionDividerWrapper}>
+      <div className={styles.sectionDivider}></div>
+      </div>
+
+      <div className={styles.sectionWrapper}>
+      <Image src='/ebvScreenshot.png' width={900} height={500} alt="ebv project shop page" className={styles.siteScreenshots}></Image>
+                <p className={styles.infoParagraph}>
+                    sfds dsfdsfs fdsf sdfsd fdsf dsfds fdsf sdfdsf dsfd sfdsf dsfds fsd fdsfds fdsf dsfds fdsf dsfd sfds fds fdsf dsf dsfds fsd fdsf dsf sdfd sfds fdsf sdf 
+                </p>
+                
+            
+            </div>
+
             <Footer />
 
         </div>
